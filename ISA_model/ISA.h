78ac70b9c94b0faaa022bc69551a5bfabd6c760e
Abhay Mohandas <abhay171005@gmail.com>
2026-04-08 22:02:14 +0530

    update

--- ISA_model/ISA.h
+++ ISA_model/ISA.h
@@ -1,35 +1,35 @@
 #define INSTR_NOP     0x00    // No Op
-#define INSTR_JZ      0x04    // 00000100-JZ[8]   Jump to address in MA0 if Zero Flag=1
-#define INSTR_JNZ     0x08    // 00001000-JNZ[8]	Jump to address in MA0 if Zero Flag=0
-#define INSTR_JC      0x0C    // 00001100-JC[8]	Jump to address in MA0 if Carry Flag=1
-#define INSTR_JNC     0x10    // 00010000-JNC[8]  Jump to address in MA0 if Carry Flag=0
-#define INSTR_JT      0x14    // 00010100-JT[8]   Jump to address in MA0 if True Flag=1
+#define INSTR_JZ      0x04    // 00000100-JZ[8]         Jump to address in MA0 if Zero Flag=1
+#define INSTR_JNZ     0x08    // 00001000-JNZ[8]	    Jump to address in MA0 if Zero Flag=0
+#define INSTR_JC      0x0C    // 00001100-JC[8]	        Jump to address in MA0 if Carry Flag=1
+#define INSTR_JNC     0x10    // 00010000-JNC[8]        Jump to address in MA0 if Carry Flag=0
+#define INSTR_JT      0x14    // 00010100-JT[8]         Jump to address in MA0 if True Flag=1
 #define INSTR_JMP     0x18    // 00011000-JMP[8]		Jump to address in MA0
 #define INSTR_CALL    0x1C    // 00011100-CALL[8]		Call to address in MA0 and save return address in stack
 #define INSTR_RET     0x20    // 00100000-RET[8]		Return to return address saved in stack
 #define INSTR_MUL     0x24    // 00100100-MUL[8]		RC*RD=[RB:RA] and update flags
 #define INSTR_DIV     0x28    // 00101000-DIV[8]		RC/RD=[Remainer in RB][Quotient in RA] and update flags
-#define INSTR_BT      0x2C    // 00101100-BT[8]		Copy the bit value in RA at position specified by the value in RB, position value from 0 to 7
+#define INSTR_BT      0x2C    // 00101100-BT[8]		    Copy the bit value in RA at position specified by the value in RB, position value from 0 to 7
 #define INSTR_BTS     0x30    // 00110000-BTS[8]		Set bit in RA at position specified by the value in RB, position value from 0 to 7
 #define INSTR_BTC     0x34    // 00110100-BTC[8]		Clear bit in RA at position specified by the value in RB, position value from 0 to 7
 #define INSTR_ACR     0x38    // 00111000-ACR[8]		(A)dd (C)arry to (R)A, RA+CF=RA and update flags
 #define INSTR_SCR     0x3C    // 00111100-SCR[8]		(S)ub (C)arry from (R)eg, RA-CF=RA and update flags
 #define INSTR_MGS     0x40    // 01000000-MGS[8]		(M)ove (G)PR to (S)PR, move value from [RB:RA] to SRC SPR specified in RC
 #define INSTR_MSG     0x44    // 01000100-MSG[8]		(M)ove (G)PR to (S)PR, move value from SRC SPR specified in RC to [RB:RA]
-#define INSTR_FENCE   0x48    // 01001000-FENCE[8]	Used to serialise all previous memory operations
+#define INSTR_FENCE   0x48    // 01001000-FENCE[8]	    Used to serialise all previous memory operations
 #define INSTR_LWO     0x4C    // 01001100-LWO[8]		(L)oad (W)ith (O)ffset, Load value from address MA0+RA into RB
 #define INSTR_SWO     0x50    // 01010000-SWO[8]		(S)tore (W)ith (O)ffset, Store value from RB to address MA0+RA
 #define INSTR_INTR    0x54    // 01010100-INTR[8]		Signal Interrupt, Interrupt number is specified in RC
 #define INSTR_SFB     0x58    // 01011000-SFB[8]		(S)et (F)lag (B)it, set flag specified in RA
 #define INSTR_CFB     0x5C    // 01011100-CFB[8]		(C)lear (F)lag (B)it, clear flag specified in RA
-#define INSTR_CLRGP   0x60    // 01100000-CLRGP[8]	Clear all the GPRs
-#define INSTR_CLRGP   0x64    // 01100100-CLRRB[8]    Clear all the Register Banks, excluding GPRs
-#define INSTR_CFB     0x68    // 01101000-SWRB0[8]	(S)wap (W)ith (R)egister (B)ank 0, values of GPRs and Reg Bank0 is swapped
-#define INSTR_CFB     0x6C    // 01101100-SWRB1[8]	(S)wap (W)ith (R)egister (B)ank 1, values of GPRs and Reg Bank1 is swapped
-#define INSTR_CFB     0x70    // 01110000-SWRB2[8]	(S)wap (W)ith (R)egister (B)ank 2, values of GPRs and Reg Bank2  is swapped
-#define INSTR_CFB     0x74    // 01110100-CMP[8]		Compare, RB-RA and update flags
-#define INSTR_CFB     0x78    // 01111000-LDH[8]		Load half word/2 bytes from address in MA0 to [RB:RA]
-#define INSTR_CFB     0x7C    // 01111100-STH[8]		Store half word/2 bytes to address in MA0 from [RB:RA]
+#define INSTR_CLRGP   0x60    // 01100000-CLRGP[8]  	Clear all the GPRs
+#define INSTR_CLRRB   0x64    // 01100100-CLRRB[8]      Clear all the Register Banks, excluding GPRs
+#define INSTR_RFRB    0x68    // 01101000-RFRB[8]    	(R)ead (F)rom (R)egister (B)ank, values of [RB3:RB0] is read to [R3:R0]. Register Bank is pointed with the value in RA
+#define INSTR_WFRB    0x6C    // 01101100-WFRB[8]	    (W)rite (t)o (R)egister (B)ank, values of [R3:R0] is written to [RB3:RB0]. Register Bank is pointed with the value in RA
+#define INSTR_IO      0x70    // 01110000-IO[8]         Access I/O ports. I/O address in RA, value in IOR SPR. RA[7] = 0 for IN and 1 for OUT 
+#define INSTR_CMP     0x74    // 01110100-CMP[8]		Compare, RB-RA and update flags
+#define INSTR_LDH     0x78    // 01111000-LDH[8]		Load half word/2 bytes from address in MA0 to [RB:RA]
+#define INSTR_STH     0x7C    // 01111100-STH[8]		Store half word/2 bytes to address in MA0 from [RB:RA]
 
 // 10000000-11111100 [OPEX DEFINED]
 
